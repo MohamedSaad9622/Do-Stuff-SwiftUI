faf8a305a63224640cf96968be1290db062fe824
Mohamed Saad <Mohamed.Saad.Khalfallah@gmail.com>
2023-10-05 17:13:13 +0300

    Initial Commit

---
+++ Todo List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo List
+//
+//  Created by Mohamed Saad on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
